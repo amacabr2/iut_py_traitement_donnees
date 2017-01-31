6f5d9192f1825c827cd041f78cc3a640c5933d7b
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-01-31 13:41:43 +0100

    Création de divers cercles

--- Imagerie/premiereUtilisationDePIL.py
+++ Imagerie/premiereUtilisationDePIL.py
@@ -1,5 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import pylab as P
+import numpy as np
+from math import sqrt
 
 
 def f1(x):
@@ -31,17 +33,67 @@ def deuxiemeDessin():
 
 def troisiemeDessin():
     """faire un carrée"""
-    P.xlim(0, 35)
-    P.ylim(0, 40)
-    X = range(10, 26)
-    Y1 = [10 for x in X]
-    Y2 = [30 for x in X]
-    P.plot(X, Y1)
-    P.plot(X, Y2)
-    P.bar(10, 20, bottom=10, width=0.2, color='red')
-    P.bar(25, 20, bottom=10, width=0.2, color='blue')
+    P.axis("equal")
+    P.xlim(-1, 2)
+    P.ylim(-1, 2)
+    x = np.array([0, 1, 1, 0, 0])
+    y = np.array([0, 0, 1, 1, 0])
+    P.plot(x, y)
     P.show()
+
+
+def ptCercle(a, b, r, x):
+    """
+    Retourne la pssition d'un point du cercle
+    :param a:
+    :param b:
+    :param r:
+    :param x:
+    :return: y0
+    """
+    if pow(r, 2) > pow((x - a), 2):
+        return b - sqrt(pow(r, 2) - pow(x - a, 2))
+    else:
+        return b + sqrt(pow(r, 2) - pow(x - a, 2))
+
+
+def quartCercle():
+    """
+    Dessine un quart de cercle
+    """
+    X = range(1, 20)
+    Y = [ptCercle(0, 20, 20, x) for x in X]
+    P.plot(X, Y)
+    P.show()
+
+
+def cercle():
+    """
+    Dessine un cercle avec le module PIL
+    """
+    i = Image.new('L', (500, 500))
+    draw = ImageDraw.Draw(i)
+    draw.arc((150, 150, 350, 350), 0, 360, fill=128)
+    i.show(command='eog')
+
+
+def fourCercle():
+    """
+    Dessine 4 cercles chacun dans un quart du plan avec le modul PIL
+    """
+    i = Image.new('L', (500, 500))
+    draw = ImageDraw.Draw(i)
+    draw.line(((0, 250), (500, 250)), fill=128)
+    draw.line(((250, 0), (250, 500)), fill=128)
+    draw.arc((0, 0, 250, 250), 0, 360, fill=300)
+    draw.arc((250, 0, 500, 250), 0, 360, fill=300)
+    draw.arc((0, 250, 250, 500), 0, 360, fill=300)
+    draw.arc((250, 250, 500, 500), 0, 360, fill=300)
+    i.show(command='eog')
 
 #premierDessin()
 #deuxiemeDessin()
-troisiemeDessin()
+#troisiemeDessin()
+#quartCercle()
+#cercle()
+fourCercle()
