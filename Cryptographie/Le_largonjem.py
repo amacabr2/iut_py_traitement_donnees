e4eaa581a2b6f4ce2b0c0376bee084d5beedf779
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-02-10 16:10:51 +0100

    Largonjem fait ^_^

--- Cryptographie/Le_largonjem.py
+++ Cryptographie/Le_largonjem.py
@@ -1,4 +1,5 @@
 from Cryptographie.Le_carre_de_25 import stringToCharTab, charTabtoString
+from Cryptographie.chiffrement_subtitution import ALPHABET
 
 CONSONNES = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z']
 
@@ -11,3 +12,24 @@ def largonjem(message):
     tab_message.reverse()
     tab_message.append(consonne + "EM")
     return charTabtoString(tab_message)
+
+
+def isLargonjemisable(mot):
+    if mot[0].upper() in CONSONNES:
+        return True
+    return False
+
+
+def transformeTexteToLargonjem(text):
+    tab_text = text.split(' ')
+    text_crypte = ""
+    for mot in tab_text:
+        if mot[0].upper() in ALPHABET:
+            if isLargonjemisable(mot):
+                text_crypte += largonjem(mot)
+            else:
+                text_crypte += mot.upper()
+        else:
+            text_crypte += mot
+        text_crypte += " "
+    return text_crypte
