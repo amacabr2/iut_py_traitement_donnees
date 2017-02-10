bdbc14383ed939e0e7e480b1c63eb7aa1526f146
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-02-10 17:30:18 +0100

    colorArithm --> non fonctionelle

--- Cryptographie/Les_nombres_premiers.py
+++ Cryptographie/Les_nombres_premiers.py
@@ -22,3 +22,44 @@ def nbPremiersJumeaux(nbPremiers):
             lNbPremiersJumeaux.append([nbPremiers[i], nbPremiers[j]])
         i += 1
     return lNbPremiersJumeaux
+
+
+COLORS = ["\033[1;41m", "\033[31m", "\033[1;42m", "\033[1;32m", "\033[1;43m", "\033[1;33m", "\033[1;44m", "\033[1;34m",
+          "\033[1;45m", "\033[1;35m", "\033[1;46m", "\033[1;36m", "\033[1;47m", "\033[1;37m"]
+RESET = "\033[0m"
+
+
+def colorArith(nbPremiers):
+    tabStringPremiers = []
+    rangColor = 0
+    i = 0
+    j = 1
+    for k in range(2, len(nbPremiers)):
+        arithm = False
+        if nbPremiers[j] - nbPremiers[i] == nbPremiers[k] - nbPremiers[j]:
+            tabStringPremiers.append(COLORS[rangColor])
+            arithm = True
+        if nbPremiers[i] not in tabStringPremiers:
+            tabStringPremiers.append(nbPremiers[i])
+        if nbPremiers[j] not in tabStringPremiers:
+            tabStringPremiers.append(nbPremiers[j])
+        if nbPremiers[k] not in tabStringPremiers:
+            tabStringPremiers.append(nbPremiers[k])
+        if arithm:
+            tabStringPremiers += RESET
+            rangColor += 1
+            if rangColor == len(COLORS):
+                rangColor = 0
+        i += 1
+        j += 1
+
+    print(tabStringPremiers)
+    string = ""
+    for stringPremier in tabStringPremiers:
+        string += str(stringPremier)
+        if str(stringPremier).isdigit():
+            string += "; "
+    print(string)
+
+
+colorArith(cribleDEratosthene(100))
