3c5f0ccfe70db629db20aea0ea12d3e96b57d7f5
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-02-27 13:21:11 +0100

    Vernam

---
+++ Cryptographie/vernam.py
@@ -0,0 +1,18 @@
+from random import randint
+
+
+def xor(caractere, entier):
+    if caractere == str(entier):
+        return "0"
+    else:
+        return "1"
+
+
+if __name__ == '__main__':
+    message = input("Quel texte chiffrer ? ")
+    key = [randint(0, 1) for k in range(len(message))]
+    print("Clé :", key)
+    crypto = ''.join([xor(message[k], key[k]) for k in range(len(message))])
+    print("Cryptogramme :", crypto)
+    decypher = ''.join([xor(crypto[k], key[k]) for k in range(len(message))])
+    print("Déchiffrement :", decypher)
