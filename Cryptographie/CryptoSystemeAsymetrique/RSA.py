2183872d92940585eec090f39139a9e379f16b61
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-03-07 10:40:41 +0100

    clefs

--- Cryptographie/CryptoSystemeAsymetrique/RSA.py
+++ Cryptographie/CryptoSystemeAsymetrique/RSA.py
@@ -1,24 +1,21 @@
 from random import random
+import Cryptographie.NombrePremier.largeprime as largeprime
 
 
 def bezout(a, b):
-    if b == 0:
-        return (a, 1, 0)
-
-    q = a // b
-    r = a % b
-    (g, u, v) = bezout(b, r)
-    return (g, v, u - q * v)
+    coeff = largeprime.bezout(a, b)
+    return [largeprime.pgcd(a, b), coeff[0], coeff[1]]
 
 
 def clef(P1, P2):
     n = P1 * P2
     c = int(random())
-    (r, u, v) = bezout(c, (P1 - 1) * (P2 - 1))
+    r, u, v = bezout(c, (P1 - 1) * (P2 - 1))
     while r != 1:
         c += 1
-        (r, u, v) = bezout(c, (P1 - 1) * (P2 - 1))
+        r, u, v = bezout(c, (P1 - 1) * (P2 - 1))
     return [(n, c), u]
 
+
 if __name__ == '__main__':
-    print(clef(5, 7))
+    print(clef(73, 97))
