0e64be9058ea82a1da66d2d4f5b19f10c1cc8128
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-03-28 11:43:54 +0200

    contrendue -> SHA-1

---
+++ Cryptographie/FctHachage/LinkedList.py
@@ -0,0 +1,34 @@
+class LinkedList:
+    """Cette classe a été trouvé sur le net"""
+
+    def __init__(self, root):
+        self.root = root
+        self.tail = root
+
+    def toString(self):
+        s = ""
+        root = self.root
+
+        if root == None:
+            return s
+
+        while (root != None):
+            s += root.value
+            root = root.next
+        return s
+
+    def add(self, node):
+        if self.root == None:
+            self.root = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+
+
+class Node:
+    """A simple node for the list"""
+
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
--- Cryptographie/FctHachage/MD5.py
+++ Cryptographie/FctHachage/MD5.py
@@ -1,13 +1,14 @@
 import math
+from Cryptographie.FctHachage.LinkedList import LinkedList, Node
 
 
 class MD5:
-    def __init__(self):
+    def __init__(self, arg=None):
         """Initialisation pour l'implementation MD5"""
-        self.A = 0x67452301
-        self.B = 0xEFCDAB89
-        self.C = 0x98BADCFE
-        self.D = 0x10325476
+        self.H0 = 0x67452301
+        self.H1 = 0xEFCDAB89
+        self.H2 = 0x98BADCFE
+        self.H3 = 0x10325476
         self.K = [int(math.floor(abs(math.sin(i + 1)) * pow(2, 32))) for i in range(64)]
         self.R = [
             7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
@@ -16,6 +17,12 @@ class MD5:
             10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
         ]
         self.digestSize = 16
+        self.list = LinkedList(None)
+        if arg:
+            self.list.add(Node(arg, None))
+            self.hash(self.list.toString())
+        else:
+            self.hash(self.list.toString())
 
     def add0(self, message):
         """Ajoute des 0 tant qu'il le faut"""
@@ -30,3 +37,69 @@ class MD5:
     def rotationLeft(self, n, x):
         """Rotation binaire par la gauche"""
         return (x << n) | (x >> (32 - n))
+
+    def F(self, x, y, z):
+        """XY v not(X) Z"""
+        return (x & y) | ((~x) & z)
+
+    def G(self, x, y, z):
+        """XZ v Y not(Z)"""
+        return (x & z) | (y & (~z))
+
+    def H(self, x, y, z):
+        """X xor Y xor Z"""
+        return x ^ y ^ z
+
+    def I(self, x, y, z):
+        """Y xor (X v not(Z))"""
+        return y ^ (x | (~z))
+
+    def splitToBlock(self, message, n):
+        """Création de blocs de 512 bits du message subdivisé en 16 mots de 32 bits"""
+        return [message[i:i + n] for i in range(0, len(message), n)]
+
+    def traiteBlock(self, blocks):
+        """Gère les block"""
+
+        for block in blocks:
+            a = self.H0
+            b = self.H1
+            c = self.H2
+            d = self.H3
+            (e, f, g, h) = (0, 0, 0, 0)
+
+            for i in range(63):
+                if i >= 0 and i <= 15:
+                    f = self.F(b, c, d)
+                    g = i
+                elif i >= 16 and i <= 31:
+                    f = self.F(d, b, c)
+                    g = (5 * i + 1) % 16
+                elif i >= 32 and i <= 47:
+                    f = self.H(b, c, d)
+                    g = (3 * i + 5) % 16
+                elif i >= 48 and i <= 63:
+                    f = self.I(c, b, d)
+                    g = (7 * i) % 16
+                temp = d
+                d = c
+                c = b
+                b = ((a + f + self.K[i] + block[g]) * self.rotationLeft(self.R[i] + b))
+                a = temp
+
+            self.H0 += a
+            self.H1 += b
+            self.H2 += c
+            self.H3 += d
+
+        return self.H0 + self.H1 + self.H2 + self.H3
+
+
+
+if __name__ == '__main__':
+    import hashlib
+    content="Wikipedia, l'encyclopedie libre et gratuite"
+    print("La phrase qui va être hach : " + content)
+    print("Avec la méthode de python : " + str(hashlib.md5(content.encode()).hexdigest()))
+    m = MD5()
+    print("Avec ma méthode : " + str(m.md5(content)))
