63dc058d08202362cfc17c919b3378cc873f56c9
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-03-28 11:31:43 +0200

    Mise en place du sac à dos --> non fonctionnel

--- Cryptographie/CryptoAsym/Sac_à_dos.py
+++ Cryptographie/CryptoAsym/Sac_à_dos.py
@@ -27,10 +27,31 @@ def clefChiffrement(p, u, l):
     return b
 
 
+def chiffre(m, b, n):
+    c = 0
+    bits = str(bin(m))
+    bits = bits.replace('0b', '')
+    while len(bits) <= n:
+        bits = '0' + bits
+    for i in range(0, n):
+        c += int(bits[i]) * b[i]
+    return c
+
+
+def dechiffre(c, v, l):
+    return (v * c) % l
+
+
 if __name__ == '__main__':
     # Suite supercroissante choisie arbitrairement pour l'exemple.
     p = [2, 3, 6, 15, 40, 100, 215, 570]
+
     # Travail préliminaire d'Alice
     (l, u) = lu(p)
     v = calculV(u, l)
     b = clefChiffrement(p, u, l)
+    # Chiffrement de Bob
+    m = 42  # Message pour l'exemple
+    c = chiffre(m, b, len(p))
+    # Dechiffrement d'Alice
+    print(dechiffre(c, v, l))
